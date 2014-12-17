f5205cd3dfb4e1e4ee97f6d34fdc25860097654c
Mido22 <ban.mido22@gmail.com>
2014-12-17 22:38:55 +0800

    pre-restart

--- public/js/comingSoon.js
+++ public/js/comingSoon.js
@@ -3,12 +3,28 @@ var comingSoon = angular.module('comingSoon', []);
 
 comingSoon.controller('ComingSoonCtrl', function($scope, $http){
 	$scope.city = "Chennai";
-	$http.jsonp("http://ipinfo.io?callback=JSON_CALLBACK").success(function(data){
-        console.log(JSON.stringify(data));
-		$scope.city = data.city;
-    });
 	
+	
+	$http.jsonp("http://ipinfo.io?callback=JSON_CALLBACK").success(function(data){        
+		if(data.city && data.city.length > 3){
+			$scope.city = data.city;
+		}
+    });	
 });
 
 
+$(document).ready(function(){
+
+		$(".fade").click(function(){
+			$(".fade .close").click();
+		});
+	$('.tmpBtn').hide();
+	setTimeout(function(){
+		$( "a" ).click(function() {
+			$('.tmpBtn').click();		
+		});
+		
+
+	},1000);
+});
 
